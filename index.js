cc3fe90835de6b0ff73f7d20d09f98b1eebe8eff
Winwin <em.ireri41@gmail.com>
2023-06-09 13:23:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 
 // Make basic player data structure, for a new fresh save file.
 player = {}
-let matter = 1
-let matter_per_click = 1
 player.matter_per_click = ExpantaNum(1)
-player.matter = ExpantaNum(matter)
-let antimatter = 0
+player.matter = ExpantaNum(0)
 player.antimatter = ExpantaNum(0)
 function tick(){
     // thank me later
-    let anttimattergenerationyes = player.matter >= 100
+    let anttimattergenerationyes = player.matter.gte(100)
     if (anttimattergenerationyes){
     player.antimatter = player.antimatter.add(1)
     // I AM NEVER GONNA USE THE OLD METHOD OF DOING THIS AGAIN I HATE CALCULATING HOW SMALL OF A NUMBER I HAVE TO MULTIPLY BY FOR IT
@@ -20,7 +17,7 @@ setInterval(tick(), 1000)
 let saving = {}
 saving.save = function(){
     let savingdata = {}
-    savingdata.matter_per_click = 1
+    savingdata.matter_per_click = JSON.stringify(player.matter_per_click.toJSON())
     savingdata.matter = JSON.stringify(player.matter.toJSON())
     savingdata.antimatter = JSON.stringify(player.antimatter.toJSON())
     return btoa(JSON.stringify(savingdata))
@@ -29,14 +26,15 @@ saving.load = function(savefile){
      let playerlip = JSON.parse(atob(savefile))
      playerlip.matter = player.matter.constructor.fromJSON(playerlip.matter)
      playerlip.antimatter = player.antimatter.constructor.fromJSON(playerlip.antimatter)
+     playerlip.matter_per_click = player.matter_per_click.constructor.fromJSON(playerlip.matter_per_click)
     player = playerlip
 }
 // Make the button in index.html increase matter.
 function matterclick()
 {
-    if (matter == 1) news.begin();
-    player.matter =  player.matter.add(player.matter_per_click.add( player.antimatter.root(player.matter)))
+    player.matter =  player.matter.add(player.matter_per_click.add(player.antimatter.root(player.matter)))
     player.matter_per_click = 1
     document.getElementById("showmatter").innerText = player.matter.toHyperE().toString()
     document.getElementById("showantimatter").innerText = player.antimatter.toHyperE().toString()
 }
+news.begin();
